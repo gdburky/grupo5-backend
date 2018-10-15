68910fc74e6900d909e89c3c61565c6c2c98ca0a
Guillermo Burky <gdburky@uc.cl>
2018-10-15 20:22:17 -0300

    start up fix

--- grupo5_backend/app.py
+++ grupo5_backend/app.py
@@ -2,14 +2,24 @@ from flask import Flask
 
 from resources.posts import posts_api
 
+# Flask settings
+FLASK_SERVER_NAME = 'localhost:8888'
+FLASK_DEBUG = True  # Do not use debug mode in production
 
-DEBUG = True
-HOST = 80
+# Flask-Restplus settings
+RESTPLUS_VALIDATE = True
+RESTPLUS_ERROR_404_HELP = False
+
 
 app = Flask(__name__)
+
+app.config['SERVER_NAME'] = FLASK_SERVER_NAME
+app.config['RESTPLUS_VALIDATE'] = RESTPLUS_VALIDATE
+app.config['ERROR_404_HELP'] = RESTPLUS_ERROR_404_HELP
+
 app.register_blueprint(posts_api)
 
 
 
 if __name__ == '__main__':
-    app.run(debug=DEBUG, host=HOST)
+    app.run(debug=FLASK_DEBUG)
