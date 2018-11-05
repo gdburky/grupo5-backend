214e5e1c0ab7818d640fdf067039674c1b7df4fe
Christopher <cggutierrez@uc.cl>
2018-11-05 01:21:14 -0300

    fixed token

--- grupo5_backend/resources/subscriptions.py
+++ grupo5_backend/resources/subscriptions.py
@@ -7,44 +7,59 @@ API_PATH = 'http://.charette15.ing.puc.cl/api'
 
 
 class SubscriptionCollection(Resource):
-    API_PATH_SC = API_PATH + '{}'.format('/subscriptions')
+    API_PATH_SC = API_PATH + '{}'.format('/api/people/{}/subscriptions')
 
     def __init__(self):
         self.reqparse= reqparse.RequestParser()
         self.reqparse.add_argument(
+            'notification',
+            required=True,
+            help= 'No notification provided',
+            location=['form', 'json',]
+        )
+        self.reqparse.add_argument(
+            'personId',
+            required=True,
+            help= 'No personId provided',
+            location=['form', 'json',]
+        )
+        self.reqparse.add_argument(
             'postId',
             required=True,
-            help= 'No post id provided',
+            help= 'No postId provided',
             location=['form', 'json',]
         )
         super().__init__()
 
-    def get(self):
-        resp = requests.get(self.API_PATH_SC)
+    def get(self, id_):
+        token = request.args.get('access_token','')
+        resp = requests.get(self.API_PATH_SC.format(id_), params={'access_token': token})
         if resp.status_code == 200:
             return jsonify(resp.json())
-        else:
+        else: 
             abort(resp.status_code)
-
-    def post(self):
+    
+    def post(self, id_):
+        token = request.args.get('access_token','')
         args = self.reqparse.parse_args()
-        resp = requests.post(self.API_PATH_SC, data=args)
+        resp = requests.post(self.API_PATH_SC.format(id_), data=args, params={'access_token': token})
         if resp.status_code == 201:
             return jsonify(resp.json())
         else:
             abort(resp.status_code)
 
 class Subscription(Resource):
-    API_PATH_S = API_PATH + '{}'.format('/subscriptions/{}')
+    API_PATH_S = API_PATH + '{}'.format('people/{}}/subscriptions/{}')
 
-    def get(self, id_):
-        resp = requests.get(self.API_PATH_S.format(id_))
+    def get(self,personId, id_):
+        resp = requests.get(self.API_PATH_S.format(personId,id_))
         if resp.status_code == 200:
             return jsonify(resp.json())
         else:
             abort(resp.status_code)
-    def delete(self, id_):
-        resp = requests.delete(self.API_PATH_S.format(id_))
+    def delete(self,personId, id_):
+        token = request.args.get('access_token','')
+        resp = requests.delete(self.API_PATH_S.format(personId,id_),params={'access_token': token})
         if resp.status_code == 200:
             return jsonify(resp.json())
         else:
@@ -54,5 +69,5 @@ class Subscription(Resource):
 subscriptions_api = Blueprint('resources.subscriptions', __name__)
 
 api = Api(subscriptions_api)
-api.add_resource(SubscriptionCollection, '/subscriptions')
-api.add_resource(Subscription, '/subscriptions/<int:id_>')
+api.add_resource(SubscriptionCollection, 'people/<int:id_>/subscriptions')
+api.add_resource(Subscription, 'people/<int:personId>/subscriptions/<int:id_>')
